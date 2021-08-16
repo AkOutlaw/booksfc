6ebb2c531b0837bfefccb828d5cbcdbde490c86f
AkOutlaw <outlaw.mwdn@gmail.com>
2021-08-17 00:24:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -123,6 +123,6 @@ app.put("/api/users", jsonParser, function(req, res){
     }
 });
    
-app.listen(3000, function(){
+app.listen(80, function(){
     console.log("Сервер ожидает подключения...");
 });
